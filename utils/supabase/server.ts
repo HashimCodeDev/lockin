077aca3d350c4c6d76b4fd394d3dadf61760351e
Hashim <hashimmohammed212@gmail.com>
2026-04-18 17:05:22 +0530

    style: format code for consistency in middleware and server files

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -5,22 +5,22 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
 export const createClient = (
-  cookieStore: Awaited<ReturnType<typeof cookies>>,
+    cookieStore: Awaited<ReturnType<typeof cookies>>,
 ) => {
-  return createServerClient(supabaseUrl!, supabaseKey!, {
-    cookies: {
-      getAll() {
-        return cookieStore.getAll();
-      },
-      setAll(cookiesToSet) {
-        try {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            cookieStore.set(name, value, options),
-          );
-        } catch {
-          // The setAll method can run in Server Components; middleware refresh handles it.
-        }
-      },
-    },
-  });
+    return createServerClient(supabaseUrl!, supabaseKey!, {
+        cookies: {
+            getAll() {
+                return cookieStore.getAll();
+            },
+            setAll(cookiesToSet) {
+                try {
+                    cookiesToSet.forEach(({ name, value, options }) =>
+                        cookieStore.set(name, value, options),
+                    );
+                } catch {
+                    // The setAll method can run in Server Components; middleware refresh handles it.
+                }
+            },
+        },
+    });
 };
